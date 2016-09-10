9ea02e66a9f22bd07cc990119033e4a4031660b7
Greg Albers <geealbers@gmail.com>
2016-09-10 10:48:01 -0700

    Apply plugin to forms dynamically based on class
    To re-use styles and tally functionality, add "mcn-extension" to "CSS
    Class Name" under Form Settings for any form.

--- gravityforms-mcn-extension.php
+++ gravityforms-mcn-extension.php
@@ -1,12 +1,10 @@
 <?php
 /**
  * Plugin Name: Gravity Forms MCN Extension
- * Description: Functions customizing the Gravity Forms plugin
+ * Description: Adds count mechanisms to the form built for crowdsourcing the 50th anniversary video, produced in the fall of 2016. To re-use styles and tally functionality, add "mcn-extension" to "CSS Class Name" under Form Settings for your form.
  * Author: Greg Albers
  * Version: 0.1
  *
- * Specifically, this adds count mechanisms to the form built for crowdsourcing
- * the 50th aniversary video, produced in the fall of 2016.
  *
  */
 
@@ -17,8 +15,8 @@ function register_plugin_styles() {
 	wp_enqueue_style( 'gravityforms-mcn-extension' );
 };
 
- add_filter( 'gform_field_choice_markup_pre_render_1', function ( $choice_markup, $choice, $field, $value ) {
-    if ( $field->get_input_type() == 'radio' ) {
+ add_filter( 'gform_field_choice_markup_pre_render', function ( $choice_markup, $choice, $field, $value ) {
+    if ( $form['cssClass'] = 'mcn-extension' && $field->get_input_type() == 'radio' ) {
 			  $search_criteria['status'] = 'active';
         $search_criteria['field_filters'][] = array( 'key' => '1', 'value' => $choice['text'] );
         $entry_counts = GFAPI::count_entries( 1, $search_criteria );
